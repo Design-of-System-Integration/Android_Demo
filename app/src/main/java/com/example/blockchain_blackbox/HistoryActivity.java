16a360b535856afb83cb0f338306d1929d907158
lucy <lucy1287@naver.com>
2023-05-02 23:59:41 +0900

    update

--- app/src/main/java/com/example/blockchain_blackbox/HistoryActivity.java
+++ app/src/main/java/com/example/blockchain_blackbox/HistoryActivity.java
@@ -1,13 +1,45 @@
 package com.example.blockchain_blackbox;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HistoryActivity extends AppCompatActivity {
+    Button btn_show_his;
+    Button btn_home;
+    Button btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_his_list);
+
+        btn_show_his = findViewById(R.id.btn_show_his);
+        btn_back = findViewById(R.id.btn_back);
+        btn_home = findViewById(R.id.btn_home);
+
+        btn_show_his.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HistoryActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HistoryActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HistoryActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
