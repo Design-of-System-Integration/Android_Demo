660484d052dc5181cbf8ec7ff3b8e3ad7ff47bc6
chaewoniii <sea9392@gmail.com>
2023-05-04 03:01:24 +0900

    chaew0nii connect except login&loading

--- app/src/main/java/com/example/blockchain_blackbox/DrivingListActivity.java
+++ app/src/main/java/com/example/blockchain_blackbox/DrivingListActivity.java
@@ -7,18 +7,17 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DrivingActivity extends AppCompatActivity {
-    Button btn_start_drive;
+public class DrivingListActivity extends AppCompatActivity {
+    Button btn_drive_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_drive);
-        btn_start_drive = findViewById(R.id.btn_start_drive);
-
-        btn_start_drive.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_start_list);
+        btn_drive_list = findViewById(R.id.btn_drive_list);
+        btn_drive_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DrivingActivity.this, DrivingListActivity.class);
+                Intent intent = new Intent(DrivingListActivity.this, DrivingListDetailActivity.class);
                 startActivity(intent);
             }
         });
